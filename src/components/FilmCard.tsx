7b0d6b2454f4ba85f2d0e3287c085e92dd895234
ddan25 <peterortiz97@yahoo.com>
2024-11-13 13:58:31 -0800

    spaces

--- src/components/FilmCard.tsx
+++ src/components/FilmCard.tsx
@@ -5,6 +5,7 @@ import { ImCross } from 'react-icons/im';
 import { CgPlayListAdd } from 'react-icons/cg';
 
 
+
 type FilmCardProps = {
   currentFilm: Film;
   addToWatchList?: (() => void) | null;
